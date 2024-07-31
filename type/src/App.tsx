09654faaa1516eae5167980009a2e2657cdf0f5f
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-07-31 09:48:34 +0100

    started with types

--- type/src/App.tsx
+++ type/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+//tuple
+let role: [number, string] = [1, "admin"];
+// it assigns type to every element in the array
+
+let myObj: object = { name: "John", age: 30 };
+//this sets a custom type
+type person = { name: string; age: number };
+//in order to make a partiular property optional, you can use the question mark
+type newguy = {name: string, age?:number}
+
+let anotherObj: person = { name: "John", age: 30 };
+//to make something share multiple types, you can use the |
+let multTypes: string | number = 20;
+multTypes = 12
+multTypes = 'akwj'
+
+//to define a function type
+let printName: Function;
+// another way to define it and its parameters
+let printerName: (name: string) => void;
+//the void is the return type of the function
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const myArray: number[] = [1, 2, 3, 4, 5];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
